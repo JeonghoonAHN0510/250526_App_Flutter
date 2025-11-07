6680a93681d17ed8111b6cfe5b8aeed86b49ac09
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-11-07 10:28:41 +0900

    feat : day110 Flutter - 페이지 전환

---
+++ lib/example/day110/main.dart
@@ -0,0 +1,24 @@
+import 'package:app_flutter/example/day110/pages/HomePage.dart';
+import 'package:app_flutter/example/day110/pages/MainPage.dart';
+import 'package:flutter/material.dart';
+
+void main(){        // Flutter 프로그램 실행 시작점
+  runApp(MyApp());  // runApp() : Flutter 앱을 시작하는 함수
+                    // MyApp() : 내가 만든 최초 위젯
+} // main end
+
+// 앱 전체의 루트 위젯 정의
+class MyApp extends StatelessWidget{
+  // StatelessWidget : 상태가 없는 위젯으로부터 상속받기
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: "/",    // 앱을 시작하는 최초 경로 지정
+                            // routes: {"경로명" : (context) => 위젯명()},
+      routes: {             // 경로와 해당 위젯을 연결하는 Map 구조
+        "/" : (context) => MainPage(),
+        "/home" : (context) => HomePage()
+      },
+    ); // MaterialApp end
+  } // build end
+} // class end
---
+++ lib/example/day110/pages/MainPage.dart
@@ -0,0 +1,40 @@
+import 'package:app_flutter/example/day110/pages/HomePage.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget{
+  // StatefulWidget : 상태가 있는 위젯으로부터 상속받기
+
+  @override
+  MainPageState createState() => MainPageState();
+} // class end
+
+class MainPageState extends State<MainPage>{
+  int currentPageIndex = 0;     // 현재 화면에 보이는 페이지 번호
+  dynamic pages = [             // == List<Widget>
+    HomePage(),                 // index 0 == 홈페이지
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: currentPageIndex,
+        children: pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        // 현재 선택된 버튼 번호 설정
+        currentIndex: currentPageIndex,
+        onTap: (index) => {setState(() {
+          currentPageIndex = index;
+        })},        // onTap : 바텀 메뉴의 하나의 탭을 눌렀을 때
+        items: [    // items : 바텀 메뉴에 들어갈 위젯 버튼들
+          BottomNavigationBarItem(    // 0번 index
+            icon: Icon(Icons.home),
+            label: "홈"
+          ),
+
+        ] // items end
+      ),
+    );
+  } // build end
+} // class end
