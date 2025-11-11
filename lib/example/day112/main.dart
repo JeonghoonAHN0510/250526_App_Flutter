e9e897b4516873bf1f0faecdef476b8165803991
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-11-11 10:37:07 +0900

    feat : day112 - day111의 todo와 연동

---
+++ lib/example/day112/main.dart
@@ -0,0 +1,21 @@
+// React에서의 Router 역할
+
+import 'package:app_flutter/example/day112/pages/Home.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+} // main end
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: "/",
+      routes: {
+        // "경로" : (context) => 위젯명(),
+        "/" : (context) => Home()
+      },
+    );
+  } // build end
+} // class end
---
+++ lib/example/day112/pages/Home.dart
@@ -0,0 +1,71 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+final dio = Dio();
+
+class Home extends StatefulWidget{
+  HomeState createState() => HomeState();
+} // class end
+
+class HomeState extends State<Home>{
+  // 1. 위젯이 최초로 실행되면 1번 실행하는 함수
+  void initState(){
+    findAll();
+  } // func end
+  // 2. Spring 서버로부터 todo 전체 목록 가져오기
+  void findAll() async {
+    try {
+      final response = await dio.get("http://192.168.40.190:8080/api/todo");
+      final data = await response.data;
+      print(data);
+      setState(() {
+        todoList = data;
+      });
+    } catch (error) {
+      print(error);
+    } // try-catch end
+  } // func end
+  // 3. 가져온 todo 목록
+  List<dynamic> todoList = [];
+  // 4. Spring 서버로부터 todo 개별 삭제
+  void deleteById(int id) async {
+    try {
+      final response = await dio.delete("http://192.168.40.190:8080/api/todo?id=${id}");
+      final data = await response.data;
+      if (data == true) findAll();
+    } catch (error) {
+      print(error);
+    } // try-catch end
+  } // func end
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("TODO"),),
+      body: Center(     // 가운데 정렬
+        child: Column(  // 세로형 위젯
+          children: [
+            Expanded(   // 확장형 세로 스크롤 위젯 지원
+              child: ListView(
+                children:
+                // 반복문을 이용하여 여러 개의 Card 대입
+                // map은 dynamic 타입을 지원하지 않는다.
+                // todoList.map((반복변수명){return 반환위젯()}).toList()
+                // 또는 day108_example2.dart 참고하여 builder로 만들기
+                  todoList.map((todo){
+                    return Card(child: ListTile(
+                      title: Text(todo['title']),
+                      subtitle: Text(todo['content']),
+                    ));
+                  }).toList()
+                ,
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  } // build end
+} // class end
